ee924bf90e25abf60f2c9e54036cf258e95c61f4
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-09 15:35:03 -0500

    Ej7 validar entrada de numero mayor que 0 terminado

---
+++ src/dev/j3c/ejercicio7/Principal.java
@@ -0,0 +1,36 @@
+package dev.j3c.ejercicio7;
+
+import javax.swing.*;
+import java.util.InputMismatchException;
+
+public class Principal {
+
+    private static double ingresarNumero() {
+        Double numero = null;   //Wrapper class (Autoboxing operation) - Clase envolvente
+        do {
+            try {
+                numero = Double.parseDouble(JOptionPane.showInputDialog("Ingrese un número para verificar si es mayor a cero: "));
+            } catch (NumberFormatException e1) {
+                JOptionPane.showMessageDialog(null,"Excepción atrapada: " + e1.getMessage());
+            } catch(InputMismatchException e2) {
+                JOptionPane.showMessageDialog(null,"Excepción atrapada: " + e2.getMessage());
+            }
+        } while(numero == null);
+        return(numero);
+    }
+
+    private static boolean esMayorQueCero(double numero) {
+        return (numero >= 0) ? true: false;
+    }
+
+    public static void main(String[] args) {
+        Double numero;
+        do {
+            numero = Principal.ingresarNumero();
+            if(!Principal.esMayorQueCero(numero)) JOptionPane.showMessageDialog(null,numero + " es menor que cero, ingrese otro número.");
+        } while(numero == null || !Principal.esMayorQueCero(numero));
+        System.out.println("\nEjercicio #7: El número: " + numero + " es válido, ya que es mayor a cero.");
+    }
+
+
+}
