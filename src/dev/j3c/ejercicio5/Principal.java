c81c55edeeccd7ab65d2cc00e187ff4c2d900531
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-09 15:22:07 -0500

    Ej5 imprimir numeros impares del 1 al 100 terminado

---
+++ src/dev/j3c/ejercicio5/Principal.java
@@ -0,0 +1,19 @@
+package dev.j3c.ejercicio5;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        int num = 2;
+        System.out.println("\nEjercicio #5: La lista de los números impares del 1 al 100, con ambos incluidos es: \n");
+        System.out.print("1" + " ");
+        while (num < 100) {
+            if(num %2 != 0) {
+                System.out.print(num + " ");
+            }
+            num++;
+        }
+        System.out.print("100" + " ");
+    }
+
+
+}
